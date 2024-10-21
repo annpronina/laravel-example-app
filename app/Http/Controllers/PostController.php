eca7b0e4ec0c032242d73854bab246a9d80c5543
unknown <your@email.example>
2024-10-21 14:24:46 +0300

    create form added

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,17 +7,39 @@ use App\Models\Post;
 
 class PostController extends Controller
 {
-    public function index(){
+    public function index()
+    {
 
         $posts = Post::all();
         return view('posts.index', ['allPosts' => $posts]);
         //return "<h1>All posts</h1>";
     }
 
-    public function show($id){
+    public function show($id)
+    {
 
-       // \Log::debug($id);
-       $post = Post::find($id);
-       return view('posts.show', ['post' => $post]);
+        // \Log::debug($id);
+        $post = Post::find($id);
+        return view('posts.show', ['post' => $post]);
+    }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store(Request $request)
+    {
+        \Log::debug($request);
+        $data = [
+            'title' => $request->title,
+            'content' => $request->content
+        ];
+
+        Post::create($data);
+
+        //return view('posts.index');
+
+        return redirect('/posts');
     }
 }
